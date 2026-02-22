4800ee71068b289452aaefcd938ad0478c529a72
KarlinCoder <giancarlo.frontend@gmail.com>
2026-02-22 16:11:27 -0500

    update

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,12 @@
 import express from "express";
 import morgan from "morgan";
+import cors from "cors";
 import { deezerRouter } from "./routes/deezer.routes";
 
 const app = express();
 
 app.use(morgan("dev"));
+app.use(cors());
 
 app.get("/", (req, res) => {
   res.json({ status: 400, message: "live" });
