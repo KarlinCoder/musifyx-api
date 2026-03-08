f7c3c59d7bb6be7dde95d88f4308869e7e9dbde5
KarlinCoder <giancarlo.frontend@gmail.com>
2026-03-08 01:44:25 -0400

    update

--- src/controllers/download.controller.ts
+++ src/controllers/download.controller.ts
@@ -1,18 +1,26 @@
-import { exec } from "child_process";
 import { Request, Response } from "express";
 import { promisify } from "util";
 import path from "node:path";
+import { spawn } from "node:child_process";
 
-const execPromise = promisify(exec);
 const scriptPath = path.resolve(process.cwd(), "python", "download.py");
 console.log(scriptPath);
 
 export const downloadAlbum = async (req: Request, res: Response) => {
   const albumId = Number(req.params.id);
-  const { stdout } = await execPromise(
-    `python3 ${scriptPath} album ${albumId}`,
-  );
-  res.json({ album_url: stdout });
+  const command = `python3 ${scriptPath} album ${albumId}`;
+
+  let response = "";
+
+  const process = spawn(command);
+
+  process.stdout.on("data", (data: string) => {
+    response += data;
+  });
+
+  process.on("error", () => {
+    res.json({ album_url: response });
+  });
 };
 
 export const downloadTack = async (req: Request, res: Response) => {
