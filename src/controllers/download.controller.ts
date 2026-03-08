f376a1b54d37c91710fbd05e87a25b677c2bc928
KarlinCoder <giancarlo.frontend@gmail.com>
2026-03-08 01:48:17 -0400

    update

--- src/controllers/download.controller.ts
+++ src/controllers/download.controller.ts
@@ -7,15 +7,15 @@ const scriptPath = path.resolve(process.cwd(), "python", "download.py");
 console.log(scriptPath);
 
 export const downloadAlbum = async (req: Request, res: Response) => {
-  const albumId = Number(req.params.id);
-  const command = `python3 ${scriptPath} album ${albumId}`;
+  const albumId = req.params.id.toString();
+  // const command = `python3 ${scriptPath} album ${albumId}`;
 
   let response = "";
 
-  const process = spawn(command);
+  const process = spawn("python3", [scriptPath, "album", albumId]);
 
   process.stdout.on("data", (data: string) => {
-    response += data;
+    response += data.toString();
   });
 
   process.on("error", () => {
